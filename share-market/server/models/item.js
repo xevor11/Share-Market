059786f309391581485e2772d14367070a0bf081
Vtewari2311 <vtewari@uwm.edu>
2023-03-25 21:03:51 -0500

    Schema for Items

---
+++ share-market/server/models/item.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  status: {
+    type: String,
+    enum: ['available', 'unavailable'],
+    default: 'available',
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
